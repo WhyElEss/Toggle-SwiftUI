3a9538be89c3a4382990ddacca754a37b9400243
Yuri Stanislavsky <stanislavskiy@me.com>
2021-01-04 12:57:41 +0200

    Initial Commit

---
+++ Toggle-SwiftUI/ToggleView.swift
@@ -0,0 +1,36 @@
+//
+//  ToggleView.swift
+//  Toggle-SwiftUI
+//
+//  Created by Юрий Станиславский on 04.01.2021.
+//
+
+import SwiftUI
+
+struct ToggleView: View {
+    @State private var isOnToggle = false
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(isOnToggle ? Color.pink : Color.blue)
+                
+                Text(isOnToggle ? "This text is on PINK screen" : "This text is on BLUE screen")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            
+            Toggle(isOn: $isOnToggle, label: {
+                Text("Label")
+            })
+        }
+        .padding()
+    }
+}
+
+struct ToggleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleView()
+    }
+}
---
+++ Toggle-SwiftUI/Toggle_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toggle_SwiftUIApp.swift
+//  Toggle-SwiftUI
+//
+//  Created by Юрий Станиславский on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Toggle_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
